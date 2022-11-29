56de61c5ad802288a32c4435acf63625497e2686
Steve Barbaro <steve.barbaro@gmail.com>
2022-11-28 21:12:36 -0600

    adds arrows

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2>&rarr; Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -36,7 +36,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
+            <h2>&rarr; Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
